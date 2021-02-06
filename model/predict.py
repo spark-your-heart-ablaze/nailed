9fae3c4e74551a1ad09235fb69e7ea5c251f28c1
viktorsurface <vantek1266@gmail.com>
2021-02-06 17:37:49 +0300

    updated the handling of the links

--- model/predict.py
+++ model/predict.py
@@ -5,6 +5,7 @@ import urllib.request
 import base64
 import requests
 from matplotlib import pyplot as plt
+import shutil  # to save it locally
 
 def mean_iou(y_true, y_pred):
     yt0 = y_true[:,:,:,0]
@@ -29,7 +30,23 @@ def predict(data):
     #|
     #|
     ###          END          ###
-    img = Image.open(urllib.request.urlopen(data))
+    #example "<a href=\"https://cp.puzzlebot.top\">Ссылка</a>"
+    #%3Ca%20href%3D%22 https%3A%2F%2Fcp.puzzlebot.top%2Fchat_statistics%2Ftg_get_file.php%3Fbot_username%3Dnailed_bot%26answer_id%3D1143899%26form_id%3DWH98ARN1P7MJ8VH0%26person_id%3D359082325%26file_id%3DAgACAgIAAxkBAAICUWAenbrBERFv_39gtHOCMrKLHM2dAAJisTEbHRzwSLGUjJ8Xkb4AAaQHbZcuAAMBAAMCAAN5AAOixAYAAR4E%26gr%3D1535990968% 22%3E%D0%A1%D1%81%D1%8B%D0%BB%D0%BA%D0%B0%3C%2Fa%3E
+    print(data)
+    start = '<a href=\"'
+    end = "\">Ссылка</a>"
+    data = data[len(start):-len(end)]
+    print(data)
+
+    r = requests.get(data, stream=True)
+    # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
+    r.raw.decode_content = True
+
+    # Open a local file with wb ( write binary ) permission.
+    with open("raw_image.jpg", 'wb') as f:
+        shutil.copyfileobj(r.raw, f)
+
+    img = Image.open("raw_image.jpg")
     img_array = np.array(img.resize((192, 160)))
 
     background = Image.fromarray(img_array)
