09459e24c4ee17846d80eee982d7b2f21297e764
vantek1266@gmail.com <DhCA9X1z@cNa>
2021-05-17 21:22:44 +0300

    Tranfering to from Flask to FastAPI

--- app.py
+++ app.py
@@ -8,25 +8,46 @@ from model import database
 
 import tensorflow
 
+from fastapi import FastAPI, HTTPException
+from fastapi import Query
+from fastapi.encoders import jsonable_encoder
+from starlette.middleware.cors import CORSMiddleware
+from starlette import status
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+import re
+import pandas as pd
+import uvicorn
+
+
+#Main app
+app = FastAPI(
+    title="Nailed IT",
+    description="API для фронта")
+
+#CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # create the flask object
-app = Flask(__name__)
 
 segment_image = custom_segmentation()
 segment_image.inferConfig(num_classes=1, class_names=["BG", "nail"])
 segment_image.load_model("model/mask_rcnn_model.067-0.335795.h5")
 
+class mask_out(BaseModel):
+    mask_photo: str
 
-@app.route('/')
-def index():
-    return "Index Page туц" + tensorflow.keras.__version__
-
-
-@app.route('/prediction', methods=['GET', 'POST'])
-def prediction():
-    data = request.form.get('data')
-    user_id = request.form.get('user_id')
-    counter = request.form.get('counter')
+@app.post('/prediction', summary="Создание маски ногтей с фотографии, которую загрузил пользователь")
+async def prediction(Photo:str = Query(..., description='Оригинальное фото руки с уникальным нэймингом в формате base64')):
+    data = Photo
+    user_id = ''
+    counter = ''
     #segment_image.load_model("model/mask_rcnn_model.067-0.335795.h5")
     if data == None:
         return 'Got None туц'
@@ -36,14 +57,19 @@ def prediction():
     return str(prediction)
 
 
-@app.route('/equip', methods=['GET', 'POST'])
-def equip():
-    name = request.form.get('data')
-    template_number = request.form.get('color')
-    user_id = request.form.get('user_id')
-    counter = request.form.get('counter')
-    stamping = request.form.get('stamping_condition')
-    color = request.form.get('color_condition')
+@app.get('/equip', summary="Получение маски нужного цвета", response_model=mask_out)
+async def equip(
+        photo_name: str,
+        stamping_condition: int,
+        color_condition: int,
+        color: str
+):
+    name = photo_name
+    template_number = color
+    user_id = ''
+    counter = ''
+    stamping = stamping_condition
+    color = color_condition
     if name == None:
         return 'Got None'
     else:
@@ -52,14 +78,23 @@ def equip():
     return str(prediction)
 
 
-@app.route('/equip_stamp', methods=['GET', 'POST'])
-def equip_stamp():
-    name = request.form.get('data')
-    stamping_name = request.form.get('stamping')
-    stamping = request.form.get('stamping_condition')
-    color = request.form.get('color_condition')
-    user_id = request.form.get('user_id')
-    counter = request.form.get('counter')
+@app.get(
+    '/equip_stamp',
+    summary="Получение фотографии с наложенным стэмпингом",
+    response_model=mask_out,
+    response_description="Возвращает фото маски с нужным цветом в формате base64")
+def equip_stamp(
+        photo_name: str,
+        stamping_condition: int,
+        color_condition: int,
+        stamping: str
+):
+    name = photo_name
+    stamping_name = stamping
+    stamping = stamping_condition
+    color = color_condition
+    user_id = ''
+    counter = ''
     if name == None:
         return 'Got None'
     else:
@@ -67,8 +102,10 @@ def equip_stamp():
         prediction = equip_stamping.equip_template(name, stamping_name, stamping, color, user_id, counter)
     return str(prediction)
 
-@app.route('/download', methods=['GET', 'POST'])
-def download():
+@app.get('/download', summary="Получение всех цветов и стемпинга для выбранного салона")
+def download(
+        salon_code:int
+):
     name = request.form.get('data')
 
     if name == None:
@@ -79,5 +116,8 @@ def download():
     return str(prediction)
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, threaded=False)
+#if __name__ == "__main__":
+#    app.run(host='0.0.0.0', debug=True, threaded=False)
+
+#if __name__=="__main__":
+#    uvicorn.run("app:app",host='0.0.0.0', debug=True)
