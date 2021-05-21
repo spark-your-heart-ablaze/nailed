8d8c0c819afe4ebe2774197887c5a73d4d302fb8
vantek1266@gmail.com <DhCA9X1z@cNa>
2021-05-21 13:47:40 +0300

    fixed formating in json

--- model/database.py
+++ model/database.py
@@ -1,17 +1,18 @@
 import pandas as pd
 import json
+import sys
 
 
 def database(data):
     df = pd.read_csv('model/Sheets for Copy of nailedIT - Color number.csv', encoding='utf8')
     df = df.dropna(how='all')
     df = df[df["Салоны"].str.contains(data)]
-    #result = df.to_json(orient="index")
 
     df1 = pd.read_csv('model/Sheets for Copy of nailedIT - Стемпинг.csv', encoding='utf8')
     df1 = df1.dropna(how='all')
     df1 = df1[df1["Салоны"].str.contains(data)]
     df = df.append(df1)
-    result1 = df.to_json(orient="index")
-    
-    return json.dumps(result1, indent=4, ensure_ascii=False).encode('utf8')
+    df.reset_index(inplace=True)
+    result1 = df.to_json(orient="index", force_ascii=False, indent=4)
+
+    return result1.replace('\/', r'/')
