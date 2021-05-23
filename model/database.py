6760d95af913e008d66e761e56f130cf731b005f
vantek1266@gmail.com <DhCA9X1z@cNa>
2021-05-23 14:02:16 +0300

    changed database to filter information based on salon code not a name

--- model/database.py
+++ model/database.py
@@ -6,11 +6,11 @@ import sys
 def database(data):
     df = pd.read_csv('model/Sheets for Copy of nailedIT - Color number.csv', encoding='utf8')
     df = df.dropna(how='all')
-    df = df[df["Салоны"].str.contains(data)]
+    df = df[df["Код салона"].astype(str).str.contains(data)]
 
     df1 = pd.read_csv('model/Sheets for Copy of nailedIT - Стемпинг.csv', encoding='utf8')
     df1 = df1.dropna(how='all')
-    df1 = df1[df1["Салоны"].str.contains(data)]
+    df1 = df1[df1["Код салона"].astype(str).str.contains(data)]
     df = df.append(df1)
     df.reset_index(inplace=True)
     result1 = df.to_json(orient="index", force_ascii=False, indent=4)
